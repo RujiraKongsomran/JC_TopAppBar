9e9c1ac66f949e4a6fb2f16fc8aef8c430ade318
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-12-18 14:53:28 +0700

    Add DropdownMenu in TopAppBar

--- app/src/main/java/com/rujirakongsomran/jc_topappbar/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_topappbar/MainActivity.kt
@@ -3,22 +3,23 @@ package com.rujirakongsomran.jc_topappbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_topappbar.ui.theme.JC_TopAppBarTheme
 import kotlinx.coroutines.launch
-import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +36,7 @@ class MainActivity : ComponentActivity() {
 fun CreateTopAppBar() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
+    val expanded = remember { mutableStateOf(false) }
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -53,9 +55,34 @@ fun CreateTopAppBar() {
                     IconButton(onClick = { /* doSomething() */ }) {
                         Icon(Icons.Filled.Search, contentDescription = "Localized description")
                     }
+
+                    Box(modifier = Modifier.wrapContentSize(Alignment.TopEnd)) {
+                        IconButton(onClick = { expanded.value = true }) {
+                            Icon(
+                                Icons.Filled.MoreVert,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = expanded.value,
+                            onDismissRequest = { expanded.value = false }
+                        ) {
+                            DropdownMenuItem(onClick = { expanded.value = false }) {
+                                Text(text = "First Item")
+                            }
+                            Divider()
+                            DropdownMenuItem(onClick = { expanded.value = false }) {
+                                Text(text = "Second Item")
+                            }
+                            Divider()
+                            DropdownMenuItem(onClick = { expanded.value = false }) {
+                                Text(text = "Third Item")
+                            }
+                        }
+                    }
                 }
             )
-        },
+        }
     ) {
 
     }
